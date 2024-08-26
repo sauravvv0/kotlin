fc7b230846dafe1b7f21bf1d2f3ce582ec6ff592
sauravvv0 <saurabtomar07@gmail.com>
2024-08-26 13:06:06 +0530

    Create addition.kt

---
+++ addition.kt
@@ -0,0 +1,40 @@
+
+class person{
+    var name:String="sahbaz";
+    val age=21;
+var a=2.34;
+    var b=3;
+    fun display(){
+        println("name is $name and age is $age and addition is ${a/b}");
+    }
+}
+
+fun main() {
+
+    val obj=person();
+    obj.display();
+
+//    val pi=3.14555555543;
+//    var a=23;
+//    var b=34;
+//    println("addition of a and b is : "+(a+b))
+//    println("constat value of pi is : "+pi);
+//    b=3;
+//    var c:Int;
+//
+//   c=a+b;
+//    println("addition of a and b : "+c);
+
+    var a = 23;
+    var b = 34;
+//    var c = a > b ? a : b
+//    var ___c=0;
+//    var Int=32
+//
+//    print(___c);
+//    var String ="sa"
+    var age:Int=24;
+    println(age);
+
+
+}
