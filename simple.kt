f1045bb17d99577b3ca8d56fb5928b8c19e73626
sauravvv0 <saurabtomar07@gmail.com>
2024-08-26 13:05:25 +0530

    Create simple.kt

---
+++ simple.kt
@@ -0,0 +1,21 @@
+
+
+/*
+
+*/
+
+
+    fun main(arg:Array<String>){
+//        println("Hi i am sahbaz");
+        print("Hello")
+        print("World")
+        var a=40;
+        println(a)
+        var `try`=10;
+        var TRY=20;
+
+        println(TRY)
+        println(`try`)
+
+
+    }
