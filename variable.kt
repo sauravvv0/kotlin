e8bcfab192187cbdda12e119c9f7a8354a5dbb2c
sauravvv0 <saurabtomar07@gmail.com>
2024-08-26 13:04:34 +0530

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,65 @@
+fun main(){
+
+//    var a=12;
+//    println(a)
+//    a=24;
+//    println(a);
+//    val b=22;
+//    print(b);
+//    var name:String
+//    name="sahaz";
+
+//    println(name)
+//    val myNum1 = 35E3F
+//    val myNum2: Double = 12E4
+//    println(myNum1)
+//    println(myNum2)
+
+//    var x="""this is a
+//
+//    program in kotlin
+//    and in java it is not""";
+//    println(x)
+//
+//    var y:Char='a';
+//    println(y)
+//    var z:Boolean=true;
+//    println(z);
+//
+//    val arr= arrayOf(1,2,3,4,5);
+//   println(arr[0])
+//    println(arr[1])
+//    println(arr[2])
+
+//    val words:Array<String> =arrayOf("sahbaz","1","2");
+//println("size is : "+words.size)
+//println(words.contentToString())
+
+//    val square=Array(5){
+//        i->i*i;
+//    }
+//    println("size is : "+square.size)
+//if(words.isEmpty()){
+//    println("empty")
+//}
+//
+//    else{
+//        println("not empty")
+//    }
+//println(words.first());
+//    println(words.last());
+//    println(words.indexOf("sahbaz"))
+
+
+    val matrix:Array<Array<Int>> = arrayOf(arrayOf(1,2,3), arrayOf(4,5,6))
+
+
+
+
+
+
+
+
+
+
+}
